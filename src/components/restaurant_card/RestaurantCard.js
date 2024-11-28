671b2a496d919a119f68c8caa99332b90e6af394
Webdevprince <pr1998ince@gmail.com>
2024-11-29 01:13:24 +0530

    Restaurant Card completed with dynamic data using props

--- src/components/restaurant_card/RestaurantCard.js
+++ src/components/restaurant_card/RestaurantCard.js
@@ -2,29 +2,40 @@ import React from "react";
 import "./restaurantcard.css";
 import RatingIcon from "../../assets/svgs/rating_icon.svg";
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  const {
+    cloudinaryImageId,
+    name,
+    avgRating,
+    sla: { slaString },
+    cuisines,
+    areaName,
+  } = resData?.info;
+
   return (
     <div className="card-wrapper">
       <div className="card-image">
         <img
-          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ntcsuou2650qimnkrc3m"
+          src={
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+            cloudinaryImageId
+          }
           alt="restaurant"
           loading="lazy"
         />
       </div>
       <div className="card-information">
-        <h3 className="card-title">Accord International</h3>
+        <h3 className="card-title">{name}</h3>
         <div className="card-rating-time">
           <span className="card-rating">
             <img src={RatingIcon} alt="rating" loading="lazy" />
-            4.4
+            {avgRating}
           </span>
-          <span className="card-time">35-40 mins</span>
+          <span className="card-time">{slaString}</span>
         </div>
-        <p className="card-cuisine">
-          North Indian, South Indian, Continental, Beverages
-        </p>
-        <p className="card-area">Vishnu Nagar</p>
+        <p className="card-cuisine">{cuisines.join(", ")}</p>
+        <p className="card-area">{areaName}</p>
       </div>
     </div>
   );
